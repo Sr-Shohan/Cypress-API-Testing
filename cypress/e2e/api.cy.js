bfc423cb96ca341d85830cba54b27d8e9e100872
Shohanur Rahman <shohanur.swe@gmail.com>
2023-08-09 23:48:17 +0600

    update api TC for body and header response

--- cypress/e2e/api.cy.js
+++ cypress/e2e/api.cy.js
@@ -3,7 +3,7 @@ describe("REST API TESTING",()=>{
         cy.request('/users/3').then((response)=>{
             cy.log(JSON.stringify(response.headers))
             
-            cy.log(JSON.stringify(response.body.data.email))
+            cy.log(JSON.stringify(response.body))
             expect(response.status).to.equal(200)
         })
     })
