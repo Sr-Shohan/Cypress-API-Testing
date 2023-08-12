fd6a2bc8e658e6c2c7dc1ffa0146ea242e68ca27
Shohanur Rahman <shohanur.swe@gmail.com>
2023-08-12 16:42:54 +0600

    Update api_POST_ERROR_HANDLE.cy.js

--- cypress/e2e/api_POST_ERROR_HANDLE.cy.js
+++ cypress/e2e/api_POST_ERROR_HANDLE.cy.js
@@ -2,12 +2,13 @@ describe("API Tesitng", ()=>{
     it("POST error handling - Login", ()=>{
         cy.request({url :'/login',  method: "POST",failOnStatusCode: false,
             body:{
-                "password": "peter@klaven"
+                "email": "peter@klaven"
             } 
         }).as('login')
         cy.get('@login').its('status').should('equal', 400)
         cy.get('@login').then((res)=>{
             expect(res.body.error).equal('Missing password')
+            
         })
     })
 })
